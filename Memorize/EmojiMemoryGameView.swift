dee07ecf25171fd801b806b25a3c64f6b41158bb
Guillem <guillemchain@gmail.com>
2021-02-18 12:29:22 +0100

    ADD Pie Shape to create a pie behind cards content ADD ViewModifier Cardify to create custom card views CHANGE EmojiMemoryGameView PreviewProvider to see the SwiftUI modifications in action

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -27,28 +27,26 @@ struct CardView: View {
     
     var body: some View {
         GeometryReader { geometry in
+            body(for: geometry.size)
+        }
+    }
+    
+    @ViewBuilder
+    private func body(for size: CGSize) -> some View{
+        if card.isFaceUp || !card.isMatched {
             ZStack {
-                if card.isFaceUp {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                    Text(card.content)
-                } else {
-                    if !card.isMatched {
-                        RoundedRectangle(cornerRadius: cornerRadius).fill()
-                    }
-                }
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding().opacity(0.4)
+                Text(card.content).font(Font.system(size: fontSize(for: size)))
+
             }
-            .font(Font.system(size: fontSize(for: geometry.size)))
+            .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
     // MARK: - Drawing Contants
-
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3.0
     
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
@@ -62,8 +60,13 @@ struct CardView: View {
 
 
 
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
